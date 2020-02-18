c7ed30f8cd15cdfdd8d7c52cbcf954f873f50db9
leeokhee <pixellog@gmail.com>
2020-02-18 13:33:54 +0900

    .

--- src/pages/Guide.vue
+++ src/pages/Guide.vue
@@ -6,19 +6,16 @@
             <ol>
                 <li><router-link to="https://bootstrap-vue.js.org">BootstrapVue</router-link></li>
                 <li><router-link to="https://gs-shop.github.io/vue-slick-carousel/#/example/simple">vue-slick-carousel</router-link></li>
-                <li><router-link to=""></router-link></li>
-                <li><router-link to=""></router-link></li>
             </ol>
             <h2>페이지</h2>
-            <ul>
+            <ol>
                 <li><router-link to="/">main</router-link></li>
                 <li><router-link to="/login">login</router-link></li>
                 <li><router-link to="/intro">intro</router-link></li>
                 <li><router-link to="/intro-join">intro-join</router-link></li>
                 <li><router-link to="/join">join</router-link></li>
                 <li><router-link to="/profile">profile</router-link></li>
-                <li><router-link to="/main">main</router-link></li>
-            </ul>
+            </ol>
         </div>
         <Intro/>
         <Login/>
